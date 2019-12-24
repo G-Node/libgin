ed7865070f4bbfb83a4891258fe245876bc67a26
Achilleas Koutsou <achilleas.k@gmail.com>
2019-12-24 16:15:16 +0100

    Copy files to new zip and replaced pointer files with content
    Create a temporary file for the new zip then replace the original.

--- cmd/archive/main.go
+++ cmd/archive/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"archive/zip"
+	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -48,6 +50,124 @@ func readAnnexedStubs(archivepath string) (map[string]string, error) {
 	return annexContentMap, nil
 }
 
+func replaceStubs(archivepath string, repopath string, annexContent map[string]string) error {
+	fmt.Println("==== Replacing pointer files with content ====")
+	fmt.Println(archivepath)
+
+	archiveloc, archivename := filepath.Split(archivepath)
+	tmpfile, err := ioutil.TempFile(archiveloc, fmt.Sprintf(".*_%s", archivename))
+	if err != nil {
+		fmt.Printf("ERROR: failed to create temporary archive: %s", err.Error())
+		return err
+	}
+
+	fmt.Printf("Created temporary file %q\n", tmpfile.Name())
+	defer tmpfile.Close()
+
+	zipWriter := zip.NewWriter(tmpfile)
+	zipReader, err := zip.OpenReader(archivepath)
+	if err != nil {
+		return err
+	}
+
+	zipcp := func(file *zip.File) error {
+		fmt.Printf("Copying %s\n", file.Name)
+		buf := make([]byte, 10240)
+		rc, err := file.Open()
+		if err != nil {
+			fmt.Printf("Failed to read: %s\n", err.Error())
+			return err
+		}
+		reader := bufio.NewReader(rc)
+		fh, err := zip.FileInfoHeader(file.FileInfo())
+		if err != nil {
+			fmt.Printf("ERROR: failed to create file header: %s\n", err.Error())
+			return err
+		}
+		// set name to zip-relative path
+		fh.Name = file.Name
+		writer, err := zipWriter.CreateHeader(fh)
+		if err != nil {
+			fmt.Printf("ERROR: failed to write header: %s\n", err.Error())
+			return err
+		}
+		for rn, err := reader.Read(buf); rn > 0 && err == nil; rn, err = reader.Read(buf) {
+			fmt.Printf("Read %d bytes\n", rn)
+			wn, werr := writer.Write(buf[:rn])
+			if werr != nil {
+				fmt.Printf("ERROR: failed to write to new zip file: %s\n", werr.Error())
+				return err
+			}
+			fmt.Printf("Wrote %d bytes\n", wn)
+		}
+		return nil
+	}
+
+	zipreplace := func(file *zip.File, annexkey string) error {
+		loc, err := git.NewCommand("annex", "contentlocation", annexkey).RunInDir(repopath)
+		if err != nil {
+			fmt.Printf("ERROR: couldn't find content file %q\n", annexkey)
+			return err
+		}
+		loc = strings.TrimRight(loc, "\n")
+		loc = filepath.Join(repopath, loc)
+
+		fmt.Printf("Replacing %q with %q\n", file.Name, loc)
+		buf := make([]byte, 10240)
+		rc, err := os.Open(loc)
+		if err != nil {
+			fmt.Printf("Failed to read: %s\n", err.Error())
+			return err
+		}
+		reader := bufio.NewReader(rc)
+		fh, err := zip.FileInfoHeader(file.FileInfo())
+		if err != nil {
+			fmt.Printf("ERROR: failed to create file header: %s\n", err.Error())
+			return err
+		}
+		// set name to zip-relative path
+		fh.Name = file.Name
+		// copy mode from content file
+		fmt.Printf("Changing mode from %d ", fh.Mode())
+		rcInfo, _ := rc.Stat()
+		fh.SetMode(rcInfo.Mode())
+		fmt.Printf("to %d\n", fh.Mode())
+		writer, err := zipWriter.CreateHeader(fh)
+		if err != nil {
+			fmt.Printf("ERROR: failed to write header: %s\n", err.Error())
+			return err
+		}
+		for rn, err := reader.Read(buf); rn > 0 && err == nil; rn, err = reader.Read(buf) {
+			fmt.Printf("Read %d bytes\n", rn)
+			wn, werr := writer.Write(buf[:rn])
+			if werr != nil {
+				fmt.Printf("ERROR: failed to write to new zip file: %s\n", werr.Error())
+				return err
+			}
+			fmt.Printf("Wrote %d bytes\n", wn)
+		}
+		return nil
+	}
+
+	for _, file := range zipReader.File {
+		annexkey, ok := annexContent[file.Name]
+		if ok {
+			zipreplace(file, annexkey)
+		} else {
+			zipcp(file)
+		}
+	}
+
+	// TODO: check errors
+	zipReader.Close()
+	zipWriter.Close()
+
+	// Overwrite original zip with temp
+	os.Rename(tmpfile.Name(), archivepath)
+
+	return nil
+}
+
 func ginarchive(path string) error {
 	repo, err := git.OpenRepository(path)
 	if err != nil {
@@ -73,21 +193,13 @@ func ginarchive(path string) error {
 		return err
 	}
 
-	for fname, annexkey := range annexContent {
-		fmt.Printf("%q -> %q\n", fname, annexkey)
-		loc, err := git.NewCommand("annex", "contentlocation", annexkey).RunInDir(path)
-		loc = strings.TrimRight(loc, "\n")
-		if err != nil {
-			fmt.Printf("ERROR: couldn't find content for file %q (%s)\n", fname, annexkey)
-			continue
-		}
-
-		fmt.Printf("%q will be replaced with %q\n", fname, loc)
+	// 3. Update git archive with annexed content
+	err = replaceStubs(archivepath, path, annexContent)
+	if err != nil {
+		fmt.Printf("ERROR: failed to update zip archive: %s", err.Error())
 	}
 
-	// 3. Update git archive with annexed content
-
-	return nil
+	return err
 }
 
 func isDirectory(path string) bool {
