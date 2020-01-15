a1663c451465b267492bfda9f5a5eeefba84b8bc
Achilleas Koutsou <achilleas.k@gmail.com>
2020-01-15 19:03:58 +0100

    Update mod and vendor

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/G-Node/libgin
 go 1.12
 
 require (
-	github.com/G-Node/git-module v0.8.4-gnode
+	github.com/G-Node/git-module v0.8.4-gnode4
 	github.com/gogs/git-module v0.8.3
 )
--- vendor/github.com/G-Node/git-module/commit_archive.go
+++ vendor/github.com/G-Node/git-module/commit_archive.go
@@ -6,11 +6,8 @@ package git
 
 import (
 	"fmt"
-	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/G-Node/libgin/libgin"
 )
 
 type ArchiveType int
@@ -28,26 +25,6 @@ func (c *Commit) CreateArchive(target string, archiveType ArchiveType, cloneL st
 		format = "zip"
 	case ArchiveTarGz:
 		format = "tar.gz"
-	case ArchiveGIN:
-		// tmppath := setting.Repository.Upload.TempPath // Circular module dependency
-		tmppath := "/data/tmp/uploads" // live config location
-		to := filepath.Join(tmppath, "archives", filepath.Base(strings.TrimSuffix(c.repo.Path, ".git")))
-		defer os.RemoveAll(to)
-		_, err := NewCommand("clone", c.repo.Path, to).RunTimeout(-1)
-		if err != nil {
-			return err
-		}
-		_, err = NewCommand("remote", "set-url", "origin", cloneL).RunInDirTimeout(-1, to)
-		if err != nil {
-			return err
-		}
-		fp, err := os.Create(target)
-		defer fp.Close()
-		if err != nil {
-			return err
-		}
-		err = libgin.MakeZip(fp, to)
-		return err
 	default:
 		return fmt.Errorf("unknown format: %v", archiveType)
 	}
