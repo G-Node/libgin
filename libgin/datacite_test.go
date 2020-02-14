d4fec0007fdd6f31df81620e2de0983a9a47f542
Achilleas Koutsou <achilleas.k@gmail.com>
2020-02-14 17:07:40 +0100

    DataCite Test: Create DataCite from YAML struct

--- libgin/datacite_test.go
+++ libgin/datacite_test.go
@@ -26,6 +26,61 @@ func Test_DataCiteMarshal(t *testing.T) {
 	example.AddReference(&Reference{ID: "arxiv:10.2222/example.doi", Reftype: "IsSupplementTo", Name: "Some other work"})
 	example.AddReference(&Reference{ID: "doi:10.3333/example.doi", Reftype: "IsReferencedBy", Name: "A work that references this dataset."})
 
+	_, err := xml.MarshalIndent(example, "", "\t")
+	if err != nil {
+		t.Fatalf("Failed to marshal: %v\n", err)
+	}
+
+	// fmt.Println(xml.Header + string(dataciteXML))
+}
+
+func Test_DataCiteFromRegInfo(t *testing.T) {
+	authors := []Author{
+		Author{
+			FirstName:   "GivenName1",
+			LastName:    "FamilyName1",
+			Affiliation: "Affiliation1",
+			ID:          "ORCID:0000-0001-2345-6789",
+		},
+		Author{
+			FirstName:   "GivenName2",
+			LastName:    "FamilyName2",
+			Affiliation: "Affiliation2",
+			ID:          "ResearcherID:X-1234-5678",
+		},
+		Author{
+			FirstName: "GivenName3",
+			LastName:  "FamilyName3",
+		},
+	}
+	references := []Reference{
+		Reference{
+			ID:      "doi:10.xxx/zzzz",
+			Reftype: "IsSupplementTo",
+			Name:    "PublicationName1",
+		},
+		Reference{
+			ID:      "arxiv:mmmm.nnnn",
+			Reftype: "IsDescribedBy",
+			Name:    "PublicationName2",
+		},
+		Reference{
+			ID:      "pmid:nnnnnnnn",
+			Reftype: "IsReferencedBy",
+			Name:    "PublicationName3",
+		},
+	}
+	regInfo := &DOIRegInfo{
+		Authors:      authors,
+		Title:        "This is a sample",
+		Description:  "This is the abstract",
+		Keywords:     []string{"Neuroscience", "Electrophysiology"},
+		License:      &License{"Creative Commons CC0 1.0 Public Domain Dedication", "https://creativecommons.org/publicdomain/zero/1.0/"},
+		Funding:      []string{"DFG, DFG.12345", "EU, EU.12345"},
+		References:   references,
+		ResourceType: "Dataset",
+	}
+	example := NewDataCiteFromRegInfo(regInfo)
 	dataciteXML, err := xml.MarshalIndent(example, "", "\t")
 	if err != nil {
 		t.Fatalf("Failed to marshal: %v\n", err)
