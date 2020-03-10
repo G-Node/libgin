082ba346a8d88d52dbd2e44fe3c86854733ac7a0
Achilleas Koutsou <achilleas.k@gmail.com>
2020-03-10 22:57:23 +0100

    XML namespace attribute fixes
    It seems that unmarshaling XML data with attributes that have custom
    namespace prefixes isn't fully supported:
    https://github.com/golang/go/issues/9519
    
    Adjusted the top-level tag's (resource) namespace attribute to be
    included in the XMLName field's tags.  More importantly, there's a new
    DataCite method for fixing the schema attributes, since they're not
    properly loaded when reading existing XML files.
    
    Also, a new loader function takes a filename and loads the data into a
    new DataCite struct, then runs the FixSchemaAttrs() method before
    returning the loaded structure.

--- libgin/datacite_test.go
+++ libgin/datacite_test.go
@@ -3,8 +3,12 @@ package libgin
 import (
 	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"math"
 	"math/rand"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 )
@@ -257,10 +261,77 @@ func Test_GetArchiveSize(t *testing.T) {
 	const expSize = 1559190240
 	size, err := GetArchiveSize(archiveURL)
 	if err != nil {
-		t.Fatalf("Failed to retrieve archive size for %q: %v", archiveURL, err)
+		t.Fatalf("Failed to retrieve archive size for %q: %v", archiveURL, err.Error())
 	}
 
 	if size != expSize {
 		t.Fatalf("Incorrect archive size: %d (expected) != %d", expSize, size)
+	}
+}
+
+func Test_MarshalUnmarshal(t *testing.T) {
+	example := NewDataCite()
+	example.Creators = []Creator{
+		Creator{"Achilleas", &NameIdentifier{"0010", "http://orcid.org", "ORCID"}, "University of Bob"},
+		Creator{"Bob", &NameIdentifier{"1111", "http://orcid.org", "ORCID"}, "University of University"},
+		Creator{"Orphan", nil, ""},
+	}
+
+	example.Titles = []string{"This is a sample"}
+	example.AddAbstract("This is the abstract")
+	example.RightsList = []Rights{Rights{"CC-BY", "http://creativecommons.org/licenses/by/4.0/"}}
+	example.Subjects = []string{"One", "Two", "Three"}
+	example.AddFunding("DFG, DFG.12345")
+	example.AddFunding("EU, EU.12345")
+	example.SetResourceType("Dataset")
+
+	example.AddReference(&Reference{ID: "doi:10.1111/example.doi", RefType: "IsDescribedBy", Name: "Manuscript title for reference."})
+	example.AddReference(&Reference{ID: "arxiv:10.2222/example.doi", RefType: "IsSupplementTo", Name: "Some other work"})
+	example.AddReference(&Reference{ID: "doi:10.3333/example.doi", RefType: "IsReferencedBy", Name: "A work that references this dataset."})
+
+	wdata, err := example.Marshal()
+	if err != nil {
+		t.Fatalf("Failed to marshal example data: %v", err.Error())
+	}
+
+	// Write data to temporary file and use UnmarshalFile() convenience function
+	tmpdir, err := ioutil.TempDir("", "datacite-test")
+	if err != nil {
+		t.Fatalf("Failed to create temporary directory: %v", err.Error())
+	}
+	defer os.RemoveAll(tmpdir)
+	fname := filepath.Join(tmpdir, "datacite.xml")
+	err = ioutil.WriteFile(fname, []byte(wdata), os.ModePerm)
+	if err != nil {
+		t.Fatalf("Failed to write marshalled data: %v", err.Error())
+	}
+
+	exampleRead, err := UnmarshalFile(fname)
+	if err != nil {
+		t.Fatalf("Failed to read XML file: %v", err.Error())
+	}
+
+	// marshal again and compare line by line
+	rdata, err := exampleRead.Marshal()
+	if err != nil {
+		t.Fatalf("Failed to marshal read data: %v", err.Error())
+	}
+
+	if len(wdata) != len(rdata) {
+		t.Fatalf("Written and read data have different length: %d != %d", len(wdata), len(rdata))
+	}
+
+	wdataLines := strings.Split(wdata, "\n")
+	rdataLines := strings.Split(rdata, "\n")
+
+	if len(wdataLines) != len(rdataLines) {
+		t.Fatalf("Written and read data have different number of lines: %d != %d", len(wdataLines), len(rdataLines))
+	}
+
+	for idx, rline := range rdataLines {
+		wline := wdataLines[idx]
+		if wline != rline {
+			t.Fatalf("Mismatched line at %d: %s != %s", idx, wline, rline)
+		}
 	}
 }
