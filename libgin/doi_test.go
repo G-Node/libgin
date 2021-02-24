39332fb898c17943df3de25e6d03f8678cc75387
M. Sonntag <michael.p.sonntag@gmail.com>
2021-02-24 18:32:56 +0100

    [test/doi] Add RenderAuthor test

--- libgin/doi_test.go
+++ libgin/doi_test.go
@@ -1,6 +1,7 @@
 package libgin
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -19,4 +20,61 @@ func TestRepoPathToUUID(t *testing.T) {
 	if retval != mval {
 		t.Fatalf("Expected %s but got %s", mval, retval)
 	}
+}
+
+func TestAuthor(t *testing.T) {
+	lname := "ln"
+	fname := "fn"
+	aff := "af"
+	id := "id"
+
+	var validate string
+	var auth Author
+	check := func(label string) {
+		rend := auth.RenderAuthor()
+		if validate != rend {
+			t.Fatalf("%s: Expected '%s' but got '%s'", label, validate, rend)
+		}
+	}
+
+	// Test RenderAuthor; make all expected combinations explicit
+	// No omit test
+	validate = fmt.Sprintf("%s,%s;%s;%s", lname, fname, aff, id)
+
+	auth = Author{
+		FirstName:   fname,
+		LastName:    lname,
+		Affiliation: aff,
+		ID:          id,
+	}
+	check("No omit")
+
+	// Omit ID test
+	validate = fmt.Sprintf("%s,%s;%s", lname, fname, aff)
+
+	auth = Author{
+		FirstName:   fname,
+		LastName:    lname,
+		Affiliation: aff,
+	}
+	check("Omit ID")
+
+	// Omit affiliation test
+	validate = fmt.Sprintf("%s,%s;%s", lname, fname, id)
+
+	auth = Author{
+		FirstName: fname,
+		LastName:  lname,
+		ID:        id,
+	}
+	check("Omit affiliation")
+
+	// Omit ID and affiliation test
+	validate = fmt.Sprintf("%s,%s", lname, fname)
+
+	auth = Author{
+		FirstName: fname,
+		LastName:  lname,
+	}
+	check("Omit ID/affiliation")
 }
